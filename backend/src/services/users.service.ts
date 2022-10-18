d5570c20168bec03caff944a4df7742a01553a7b
bkrisztian01 <bui.krisztian01@gmail.com>
2022-10-18 18:23:57 +0200

    Refactor users

--- backend/src/services/users.service.ts
+++ backend/src/services/users.service.ts
@@ -2,6 +2,41 @@ import type {User} from '../models/users.model';
 
 export const users: User[] = [];
 
+export function validatePassword(username: string, password: string) {
+	return users.find(u => u.username === username && u.password === password);
+}
+
 export function getUserById(id: number) {
 	return users.find(user => user.id === id);
+}
+
+export function createUser(
+	username: string,
+	password: string,
+	fullname: string,
+	email: string,
+) {
+	let user = users.find(u => u.email === email);
+	if (user) {
+		throw new Error('E-mail address is already in use');
+	}
+
+	user = users.find(u => u.username === username);
+	if (user) {
+		throw new Error('Username is already in use');
+	}
+
+	user = {
+		id: users.length + 1,
+		username,
+		password,
+		fullname,
+		email,
+	};
+	users.push(user);
+	return user;
+}
+
+export function getUsers() {
+	return users;
 }
