797985d1bd3ea7f43e9cb4d7985223f2ba5a8c35
bkrisztian01 <bui.krisztian01@gmail.com>
2022-10-23 13:58:14 +0200

    Fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import routes from './routes';
 import {initTestData} from './testData';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.PORT ?? 5000;
 
 routes(app);
