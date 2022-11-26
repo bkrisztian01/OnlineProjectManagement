89d2d2a97566f366d23f3eea67e8f80eef103972
FischerBalazs <fischerbalazs2002@gmail.com>
2022-11-26 19:20:31 +0100

    Uj API javitva, Add Task, Add Milestone KESZ,  workers/prerequisite/requiredTasks Kesz

--- frontend/src/app/charts/gantt/gantt.component.ts
+++ frontend/src/app/charts/gantt/gantt.component.ts
@@ -38,52 +38,7 @@ export class GanttComponent implements OnInit {
 
   constructor(private api: ApiService) {
     this.chartOptions = {
-      series: [
-        // {
-        //   data: [
-        //     {
-        //       x: "Analysis",
-        //       y: [
-        //         new Date("2019-02-27").getTime(),
-        //         new Date("2019-03-04").getTime()
-        //       ],
-        //       fillColor: "#008FFB"
-        //     },
-        //     {
-        //       x: "Design",
-        //       y: [
-        //         new Date("2019-03-04").getTime(),
-        //         new Date("2019-03-08").getTime()
-        //       ],
-        //       fillColor: "#00E396"
-        //     },
-        //     {
-        //       x: "Coding",
-        //       y: [
-        //         new Date("2019-03-07").getTime(),
-        //         new Date("2019-03-10").getTime()
-        //       ],
-        //       fillColor: "#775DD0"
-        //     },
-        //     {
-        //       x: "Testing",
-        //       y: [
-        //         new Date("2019-03-08").getTime(),
-        //         new Date("2019-03-12").getTime()
-        //       ],
-        //       fillColor: "#FEB019"
-        //     },
-        //     {
-        //       x: "Deployment",
-        //       y: [
-        //         new Date("2019-03-12").getTime(),
-        //         new Date("2019-03-17").getTime()
-        //       ],
-        //       fillColor: "#FF4560"
-        //     }
-        //   ]
-        // }
-      ],
+      series: [  ],
       chart: {
         height: 350,
         type: "rangeBar"
@@ -106,60 +61,61 @@ export class GanttComponent implements OnInit {
     };
   }
   ngOnInit(): void {
+    //this.getAllTasks()
     this.initializeChart()
-    this.getAllTasks()
   }
 
   initializeChart(){
     this.chartOptions = {
       series: [
-        // {
-        //   data: [
-        //     {
-        //       x: "Analysis",
-        //       y: [
-        //         new Date("2019-02-27").getTime(),
-        //         new Date("2019-03-04").getTime()
-        //       ],
-        //       fillColor: "#008FFB"
-        //     },
-        //     {
-        //       x: "Design",
-        //       y: [
-        //         new Date("2019-03-04").getTime(),
-        //         new Date("2019-03-08").getTime()
-        //       ],
-        //       fillColor: "#00E396"
-        //     },
-        //     {
-        //       x: "Coding",
-        //       y: [
-        //         new Date("2019-03-07").getTime(),
-        //         new Date("2019-03-10").getTime()
-        //       ],
-        //       fillColor: "#775DD0"
-        //     },
-        //     {
-        //       x: "Testing",
-        //       y: [
-        //         new Date("2019-03-08").getTime(),
-        //         new Date("2019-03-12").getTime()
-        //       ],
-        //       fillColor: "#FEB019"
-        //     },
-        //     {
-        //       x: "Deployment",
-        //       y: [
-        //         new Date("2019-03-12").getTime(),
-        //         new Date("2019-03-17").getTime()
-        //       ],
-        //       fillColor: "#FF4560"
-        //     }
-        //   ]
-        // }
+        {
+          data: [
+            {
+              x: "Analysis",
+              y: [
+                new Date("2019-02-27").getTime(),
+                new Date("2019-03-04").getTime()
+              ],
+              fillColor: "#008FFB"
+            },
+            {
+              x: "Design",
+              y: [
+                new Date("2019-03-04").getTime(),
+                new Date("2019-03-08").getTime()
+              ],
+              fillColor: "#00E396"
+            },
+            {
+              x: "Coding",
+              y: [
+                new Date("2019-03-07").getTime(),
+                new Date("2019-03-10").getTime()
+              ],
+              fillColor: "#775DD0"
+            },
+            {
+              x: "Testing",
+              y: [
+                new Date("2019-03-08").getTime(),
+                new Date("2019-03-12").getTime()
+              ],
+              fillColor: "#FEB019"
+            },
+            {
+              x: "Deployment",
+              y: [
+                new Date("2019-03-12").getTime(),
+                new Date("2019-03-17").getTime()
+              ],
+              fillColor: "#FF4560"
+            }
+          ]
+        }
       ],
       chart: {
         height: 350,
+        width: 700,
         type: "rangeBar"
       },
       plotOptions: {
@@ -185,21 +141,28 @@ export class GanttComponent implements OnInit {
   getAllTasks(){
     this.api.getTask(1,this.api.AccessTokenThrow()).subscribe(res=>{
       this.tasks = res;
+      console.log(this.tasks);
+      for(let row of this.tasks){
+        let name:any = row.name;
+        let y1  =''+row.startDate+'';
+        let y2 = row.deadline;
+        let color = "#FF4560"
+        let type:any = y1+","+y2
+        let data: number[] = [name,type,color]
+        // const data={
+        //   x: name,
+        //   y: type,
+        //   color: color,
+        // }
+        this.chartOptions.series?.push(
+          {name,type,color,data}
+          )
+          console.log(data);
+      }
+      console.log(this.chartOptions.series);
+      
     })
-
-    console.log(this.tasks);
     
-    for(let row of this.tasks){
-      let name:any = row.name;
-      let y1  =row.startDate;
-      let y2 = row.deadline;
-      let color = "#FF4560"
-      let type:any = y1+","+y2
-      let data: number[] = [name,type,color]
-      this.chartOptions.series?.push(
-        {name,type,color,data}
-        )
-    }
   }
 
 }
