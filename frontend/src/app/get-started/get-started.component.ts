4328b5cdb4d22585c61bd5e8680e94fa1509409a
FischerBalazs <fischerbalazs2002@gmail.com>
2022-11-30 23:42:22 +0100

    get started testing

--- frontend/src/app/get-started/get-started.component.ts
+++ frontend/src/app/get-started/get-started.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-get-started',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-started.component.css']
 })
 export class GetStartedComponent implements OnInit {
+  shown: boolean = false;
+  showntext1: boolean = false;
+  showntext2: boolean = false;
+  showntext3: boolean = false;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+
+    (async () => { 
+      await this.delay(1000);
+      this.showntext1=true;
+      await this.delay(100);
+      this.showntext2=true;
+      await this.delay(100);
+      this.showntext3=true;
+
+  })();
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+    
+}
+  CurrentDelay(){
+    (async () => { 
+      // Do something before delay
+      this.shown=true;
+      console.log('before delay')
+      
+
+      await this.delay(2700);
+
+      // Do something after
+      console.log('after delay')
+      this.router.navigate(['/login'])
+  })();
   }
 
 }
